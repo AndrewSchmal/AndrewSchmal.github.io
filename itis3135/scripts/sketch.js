37bea787da51c30fa7600540cf4fd8c6bd075113
AndrewSchmal <andrewschmal5@gmail.com>
2024-12-12 15:00:31 -0500

    drawingsss

--- itis3135/scripts/sketch.js
+++ itis3135/scripts/sketch.js
@@ -1,126 +1,48 @@
-let circleX = 200;
-let circleY = 150;
-let circleRadius = 75;
+// Source = https://p5js.org/examples/angles-and-motion-sine-cosine/    
 
-let graphX = 50;
-let graphY = 300;
-let graphAmplitude = 50;
-let graphPeriod = 300;
+const sketch1 = (p) => {
+    let circleX = 200;
+    let circleY = 150;
+    let circleRadius = 75;
 
-function setup() {
-    createCanvas(400, 400);
-    angleMode(DEGREES);
-    describe(
-        'Animated demonstration of a point moving around the unit circle, together with the corresponding sine and cosine values moving along their graphs.'
-    );
-}
+    let graphX = 50;
+    let graphY = 300;
+    let graphAmplitude = 50;
+    let graphPeriod = 300;
 
-function draw() {
-    background(0);
+    p.setup = () => {
+        let canvas = p.createCanvas(400, 400);
+        canvas.parent('drawing-container-1');
+        p.angleMode(p.DEGREES);
+    };
 
-    // Set angle based on frameCount, and display current value
+    p.draw = () => {
+        p.background(0);
+        let angle = p.frameCount % 360;
 
-    let angle = frameCount % 360;
+        // Drawing the circle
+        p.noFill();
+        p.stroke(128);
+        p.strokeWeight(3);
+        p.circle(circleX, circleY, 2 * circleRadius);
+        p.line(circleX, circleY - circleRadius, circleX, circleY + circleRadius);
+        p.line(circleX - circleRadius, circleX + circleRadius);
 
-    fill(255);
-    textSize(20);
-    textAlign(LEFT, CENTER);
-    text(`angle: ${angle}`, 25, 25);
+        // Calculate points and draw moving lines
+        let pointX = circleX + circleRadius * p.cos(angle);
+        let pointY = circleY - circleRadius * p.sin(angle);
 
-    // Draw circle and diameters
+        p.line(circleX, circleY, pointX, pointY);
+        p.noStroke();
+        p.fill('white');
+        p.circle(pointX, pointY, 10);
 
-    noFill();
-    stroke(128);
-    strokeWeight(3);
-    circle(circleX, circleY, 2 * circleRadius);
-    line(circleX, circleY - circleRadius, circleX, circleY + circleRadius);
-    line(circleX - circleRadius, circleY, circleX + circleRadius, circleY);
+        // Graph
+        let lineX = p.map(angle, 0, 360, graphX, graphX + graphPeriod);
+        p.stroke('grey');
+        p.line(lineX, graphY - graphAmplitude, lineX, graphY + graphAmplitude);
+    };
+};
 
-    // Draw moving points
+new p5(sketch1);
 
-    let pointX = circleX + circleRadius * cos(angle);
-    let pointY = circleY - circleRadius * sin(angle);
-
-    line(circleX, circleY, pointX, pointY);
-
-    noStroke();
-
-    fill('white');
-    circle(pointX, pointY, 10);
-
-    fill('orange');
-    circle(pointX, circleY, 10);
-
-    fill('red');
-    circle(circleX, pointY, 10);
-
-    // Draw graph
-
-    stroke('grey');
-    strokeWeight(3);
-    line(graphX, graphY, graphX + 300, graphY);
-    line(graphX, graphY - graphAmplitude, graphX, graphY + graphAmplitude);
-    line(
-        graphX + graphPeriod,
-        graphY - graphAmplitude,
-        graphX + graphPeriod,
-        graphY + graphAmplitude
-    );
-
-    fill('grey');
-    strokeWeight(1);
-    textAlign(CENTER, CENTER);
-    text('0', graphX, graphY + graphAmplitude + 20);
-    text('360', graphX + graphPeriod, graphY + graphAmplitude + 20);
-    text('1', graphX / 2, graphY - graphAmplitude);
-    text('0', graphX / 2, graphY);
-    text('-1', graphX / 2, graphY + graphAmplitude);
-
-    fill('orange');
-    text('cos', graphX + graphPeriod + graphX / 2, graphY - graphAmplitude);
-    fill('red');
-    text('sin', graphX + graphPeriod + graphX / 2, graphY);
-
-    // Draw cosine curve
-
-    noFill();
-    stroke('orange');
-    beginShape();
-    for (let t = 0; t <= 360; t++) {
-        let x = map(t, 0, 360, graphX, graphX + graphPeriod);
-        let y = graphY - graphAmplitude * cos(t);
-        vertex(x, y);
-    }
-    endShape();
-
-    // Draw sine curve
-
-    noFill();
-    stroke('red');
-    beginShape();
-    for (let t = 0; t <= 360; t++) {
-        let x = map(t, 0, 360, graphX, graphX + graphPeriod);
-        let y = graphY - graphAmplitude * sin(t);
-        vertex(x, y);
-    }
-    endShape();
-
-    // Draw moving line
-
-    let lineX = map(angle, 0, 360, graphX, graphX + graphPeriod);
-    stroke('grey');
-    line(lineX, graphY - graphAmplitude, lineX, graphY + graphAmplitude);
-
-    // Draw moving points on graph
-
-    let orangeY = graphY - graphAmplitude * cos(angle);
-    let redY = graphY - graphAmplitude * sin(angle);
-
-    noStroke();
-
-    fill('orange');
-    circle(lineX, orangeY, 10);
-
-    fill('red');
-    circle(lineX, redY, 10);
-}
--- itis3135/scripts/sketch2.js
+++ itis3135/scripts/sketch2.js
@@ -1,47 +1,43 @@
-function setup() {
-    createCanvas(400, 400);
-    colorMode(HSB);
-  
-    // Set angle mode so that atan2() returns angles in degrees
-    angleMode(DEGREES);
-  
-    describe('Two eyes that follow the cursor.');
-  }
-  
-  function draw() {
-    background(0);
-  
-    // Draw left eye
-  
-    let leftX = 150;
-    let leftY = 200;
-  
-    // Calculate angle between left eye and mouse
-    let leftAngle = atan2(mouseY - leftY, mouseX - leftX);
-  
-    push();
-    translate(leftX, leftY);
-    fill(255);
-    ellipse(0, 0, 50, 50);
-    rotate(leftAngle);
-    fill(0);
-    ellipse(12.5, 0, 25, 25);
-    pop();
-  
-    // Draw right eye
-  
-    let rightX = 250;
-    let rightY = 200;
-  
-    // Calculate angle between right eye and angle
-    let rightAngle = atan2(mouseY - rightY, mouseX - rightX);
-  
-    push();
-    translate(rightX, rightY);
-    fill(255);
-    ellipse(0, 0, 50, 50);
-    rotate(rightAngle);
-    fill(0);
-    ellipse(12.5, 0, 25, 25);
-    pop();
-  }
+// Source = https://p5js.org/examples/angles-and-motion-aim/
+
+const sketch2 = (p) => {
+    p.setup = () => {
+        let canvas = p.createCanvas(400, 400);
+        canvas.parent('drawing-container-2');
+        p.colorMode(p.HSB);
+        p.angleMode(p.DEGREES);
+    };
+
+    p.draw = () => {
+        p.background(0);
+        let leftX = 150;
+        let leftY = 200;
+
+        let leftAngle = p.atan2(p.mouseY - leftY, p.mouseX - leftX);
+
+        p.push();
+        p.translate(leftX, leftY);
+        p.fill(255);
+        p.ellipse(0, 0, 50, 50);
+        p.rotate(leftAngle);
+        p.fill(0);
+        p.ellipse(12.5, 0, 25, 25);
+        p.pop();
+
+        let rightX = 250;
+        let rightY = 200;
+
+        let rightAngle = p.atan2(p.mouseY - rightY, p.mouseX - rightX);
+
+        p.push();
+        p.translate(rightX, rightY);
+        p.fill(255);
+        p.ellipse(0, 0, 50, 50);
+        p.rotate(rightAngle);
+        p.fill(0);
+        p.ellipse(12.5, 0, 25, 25);
+        p.pop();
+    };
+};
+
+new p5(sketch2);
